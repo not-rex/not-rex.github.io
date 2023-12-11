4db3939717830e399fa58449726656200f755c3c
Rex <harrison010902@gmail.com>
2023-12-11 18:40:11 +0800

    new site

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import HomePage from './components/HomePage.vue'
+</script>
+
+<template>
+
+  <NavBar />
+  <HomePage />
+  
+</template>
+
+<style>
+
+body {
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: rgb(21, 21, 21);
+  color:rgb(246, 246, 233);
+  font-family:
+    Inter,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    Oxygen,
+    Ubuntu,
+    Cantarell,
+    'Fira Sans',
+    'Droid Sans',
+    'Helvetica Neue',
+    sans-serif;
+  transition:
+    color 0.5s,
+    background-color 0.5s;
+}
+
+</style>
